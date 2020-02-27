b427597445f33e5166a1e02d2c40b2d8b7192f1c
SepSol <SR-75@live.com>
2020-02-27 19:12:52 +0330

    changed function App to class App

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-function App() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return(
+            <div>
 
-  
+            <h1>hello!</h1>
+
+            </div>
+        );
+    }
+}
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
