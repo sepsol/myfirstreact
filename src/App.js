5ee52a5c81551dd3a3eb445587926c594a571fe6
SepSol <SR-75@live.com>
2020-02-27 17:04:24 +0330

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>sudo</code> and save to reload.
+          Edit <code>test</code> and save to reload.
         </p>
         <a
           className="App-link"
